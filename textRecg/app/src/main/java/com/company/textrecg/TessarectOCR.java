7fb1820a36ba699a891b4bea1562ae21a26e2e23
Radu Tudorica <radu.tudorica@info.uaic.ro>
2020-04-29 02:11:38 +0300

    terminat restructurarea cu interfata ocrInterface

--- textRecg/app/src/main/java/com/company/textrecg/TessarectOCR.java
+++ textRecg/app/src/main/java/com/company/textrecg/TessarectOCR.java
@@ -4,7 +4,11 @@ import android.app.DownloadManager;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
@@ -13,21 +17,27 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.function.Function;
 
-public class TessarectOCR {
+public class TessarectOCR implements  ocrInterface{
     private final TessBaseAPI tessarectAPI = new TessBaseAPI();
-
     public  TessarectOCR(Context app, int resourceId){
         copyPack(app, resourceId);
         tessarectAPI.init(app.getFilesDir().toString(), "eng");
     }
 
-    public void copyPack(Context app, int resurceId){
-        File dropPath = new File(app.getFilesDir().toString()+"/tessdata/");
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public String runOcrOnImage(Bitmap image){
+        tessarectAPI.setImage(image);
+        return tessarectAPI.getUTF8Text();
+    }
+
+    public void copyPack(Context app, int resurceId) {
+        File dropPath = new File(app.getFilesDir().toString() + "/tessdata/");
         System.out.println("tessdata!");
-        System.out.println(app.getFilesDir()+"/tessdata/");
-        if(dropPath.exists()){
-           return;
+        System.out.println(app.getFilesDir() + "/tessdata/");
+        if (dropPath.exists()) {
+            return;
         }
         try {
             dropPath.mkdirs();
@@ -36,7 +46,7 @@ public class TessarectOCR {
             FileOutputStream out = new FileOutputStream(app.getFilesDir().toString() + "/tessdata/eng.traineddata");
             byte[] buffer = new byte[4096];
             int bufferSize = 0;
-            while((bufferSize = in.read(buffer, 0, 4095)) > 0){
+            while ((bufferSize = in.read(buffer, 0, 4095)) > 0) {
                 out.write(buffer, 0, bufferSize);
             }
         } catch (FileNotFoundException e) {
@@ -45,11 +55,6 @@ public class TessarectOCR {
             e.printStackTrace();
         }
 
-    }
-
-    public  String detect(Bitmap image){
-        tessarectAPI.setImage(image);
-        return tessarectAPI.getUTF8Text();
     }
 
     public void onDestroy(){
---
+++ textRecg/app/src/main/java/com/company/textrecg/ocrInterface.java
@@ -0,0 +1,11 @@
+package com.company.textrecg;
+
+import android.graphics.Bitmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.function.Function;
+
+public interface ocrInterface {
+    public String runOcrOnImage(Bitmap image);
+}
